ae306c7bec220a6940d3fa0118b7871a5324ba62
Sladrus <ufimkinyasha@gmail.com>
2022-09-09 01:06:51 +0400

    fix

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -29,9 +29,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "ffa9f2cee5326d097d99f0b8ab243959",
-"/": "ffa9f2cee5326d097d99f0b8ab243959",
-"main.dart.js": "b46962004a0ccdc83406e8d0a45e9b35",
+"index.html": "225daeb902463e84b6e8e5fbdf247fce",
+"/": "225daeb902463e84b6e8e5fbdf247fce",
+"main.dart.js": "d8ba5a28514cc7b84dff0cdd3e803230",
 "manifest.json": "6a4e9cd324d72a32feda6867df80bbf5",
 "styles.css": "36de02a4e5001400b1030e57a6ca07e5",
 "version.json": "08587b001ef0011ab9281d68b7e48a99"
